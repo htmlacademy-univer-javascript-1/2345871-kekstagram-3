be073d49e22c49f3ae106a5d915c9abf0bdd3367
Pauline <makarova.pv@edu.spbstu.ru>
2023-03-13 02:48:20 +0300

    done 4.15

--- js/main.js
+++ js/main.js
@@ -1,19 +1,46 @@
-function generateRandomNumberInRange(min, max) {
-  const rand = min + Math.random() * (max - min + 1);
-  return Math.floor(rand);
+const DESCRIPTIONS = [
+  'Это я в Дубае, я сейчас отдыхаю',
+  'Я вообще делаю что хочу',
+  'Мне платят за концерты, я читаю под фанеру',
+  'Я подумаю потом, но скажу сразу - да',
+  'Пусть не забывают, кто тут королева',
+  'Эту сумку мне муж купил',
+  'На мне норковая шуба',
+  'Миллионы алых роз',
+  'Твоя любовь это так красиво',
+  'Болючая осень, холодный ноябрь',
+  'Не тешат прогнозы',
+  'Пятые сутки я не сплю',
+  'Этот пейзаж меня будоражит',
+  'Я в глазах твоих видел снег в океане',
+  'Закат',
+];
+
+function getRandomPositiveInteger(a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
+// function checkStringLength(string, length) {
+//   return string.length <= length;
+// }
 
-function checkLenght(stringToCheck, maxLength) {
-  stringToCheck = String(stringToCheck);
-  const lengthOfString = length(stringToCheck);
-  if (lengthOfString <= maxLength) {
-    return true;
-  }
-  else {
-    return false;
-  }
-}
+// for DESCRIPTION
+const getRandomArrayElement = (elements) =>
+  elements[getRandomPositiveInteger(0, elements.length - 1)];
 
-generateRandomNumberInRange();
-checkLenght();
+let idNumber = 1;
+let urlNumber = 1;
+
+const createPublication = () => ({
+  id: idNumber++,
+  url: `photos/${urlNumber++}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomPositiveInteger(15,200),
+  comments: getRandomPositiveInteger(0,200),
+});
+
+const arrayOfPublications = Array.from({length: 25}, createPublication);
+arrayOfPublications();
